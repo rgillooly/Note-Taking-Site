f8efaacdfa80fdf3e18ded1c1ca816f5321c6afa
rgillooly <rpgillooly@gmail.com>
2023-11-14 15:37:12 -0500

    fixed many issues

--- Develop/server.js
+++ Develop/server.js
@@ -1,14 +1,63 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
 
-const PORT = 3000;
+const PORT = 3001;
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/public/notes', (req, res) => {
+app.use(express.static('public'));
+
+app.get('/', (req, res) => 
+res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) => 
+res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/notes', (req, res) => {
+res.status(200),json(`${req.method} request received to get reviews`)
+
 console.info(`${req.method} request received to get notes`)
-}
-)
+})
 
-app.listen(3000);
+
+app.post('/notes', (req, res) => {
+    
+console.info(`${req.method} request received to add a note`)
+
+    const {noteTitle, noteText} = req.body;
+
+    if (noteTitle && noteText) {
+        const newNote = {
+            noteTitle,
+            noteText
+        }
+        
+            fs.readFile('../api/db.json', 'utf-8', (err, data) => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    const parsedNotes = JSON.parse(data);
+                    
+                    parsedNotes.push(newNote);
+
+                    fs.writeFile(
+                        '../api/db.json',
+                        JSON.stringify(parsedNotes, null, 2),
+                        (writeErr) =>
+                        writeErr
+                        ? console.error(writeErr)
+                        : console.info('Successfully added note!')
+                );
+                }
+            });
+        }});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
